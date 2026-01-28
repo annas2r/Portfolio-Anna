dc1c4067a432d33566a2f1a8fef32c7253a67ed3
annafs2 <anna.flavia@ribeiraodasneves.mg.gov.br>
2026-01-28 10:44:52 -0300

    Ajustes e alterações em projetos

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,14 +8,12 @@ import { ServicesList } from './components/ServicesList';
 import { Contact } from './components/Contact';
 import { Footer } from './components/Footer';
 import { CustomCursor } from './components/CustomCursor';
-import { VLibras } from './components/VLibras';
 import { AccessibilityMenu } from './components/AccessibilityMenu';
 
 export default function App() {
   return (
     <div className="min-h-screen bg-white antialiased cursor-none">
       <CustomCursor />
-      <VLibras />
       <AccessibilityMenu />
       <Toaster position="top-center" />
       <Header />
--- src/app/components/VLibras.tsx
+++ src/app/components/VLibras.tsx
@@ -2,26 +2,26 @@ import { useEffect } from 'react';
 
 export function VLibras() {
   useEffect(() => {
+    const scriptSrc = 'https://vlibras.gov.br/app/vlibras-plugin.js';
+    
+    if (document.querySelector(`script[src="${scriptSrc}"]`)) return;
+
     const script = document.createElement('script');
-    script.src = 'https://vlibras.gov.br/app/vlibras-plugin.js';
+    script.src = scriptSrc;
     script.async = true;
+    
     script.onload = () => {
       // @ts-ignore
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       new (window as any).VLibras.Widget('https://vlibras.gov.br/app');
     };
+    
     document.body.appendChild(script);
-
-    return () => {
-      if (document.body.contains(script)) {
-        document.body.removeChild(script);
-      }
-    };
   }, []);
 
   // Estrutura exigida pelo widget
   return (
-    <div className="vlibras-widget enabled">
+    <div className="vlibras-widget enabled" style={{ zIndex: 99999 }}>
       <div className="vw-access-button-active"></div>
       <div className="vw-plugin-wrapper">
         <div className="vw-plugin-top-wrapper"></div>
